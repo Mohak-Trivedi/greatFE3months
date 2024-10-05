a39027688e06682511f914393608077f51821dea
Mohak-Trivedi <trivedimohak@gmail.com>
2024-10-05 18:48:07 +0530

    solved filter polyfill

---
+++ js-implementations/filterPolyfill.js
@@ -0,0 +1,21 @@
+// Problem Link: https://www.greatfrontend.com/questions/javascript/array-filter?list=three-months
+
+/**
+ * @template T
+ * @param { (value: T, index: number, array: Array<T>) => boolean } callbackFn
+ * @param {any} [thisArg]
+ * @return {Array<T>}
+ */
+Array.prototype.myFilter = function (callbackFn, thisArg) {
+  let res = [];
+
+  for (let i = 0; i < this.length; i++) {
+    let cbVal = callbackFn.call(thisArg, this[i], i, this);
+
+    if (cbVal) {
+      res.push(this[i]);
+    }
+  }
+
+  return res;
+};
